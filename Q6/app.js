4b737d17290d3f053328608d573bbd46a2bf75ab
Manash <manash.intern@yum.si>
2023-07-12 23:32:19 +0530

    Q6

---
+++ Q6/app.js
@@ -0,0 +1,16 @@
+const showDialog = () => {
+    document.getElementById('dialog').classList.add('show')
+    document.querySelector("body").style.overflow = 'hidden';
+};
+const closeDialog = () => {
+    document.getElementById('dialog').classList.remove('show');
+    document.querySelector("body").style.overflow = 'visible';
+}
+
+window.onclick = function (event) {
+    const modal = document.getElementById('dialog');
+    if (event.target == modal) {
+        modal.classList.remove('show');
+        document.querySelector("body").style.overflow = 'visible';
+    }
+}
